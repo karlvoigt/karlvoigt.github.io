90579a94670b5742191a0d8728dce7d4aa45bfe6
Karl <360appleid@gmail.com>
2020-02-06 18:25:55 +0200

    Update events.js

--- events.js
+++ events.js
@@ -2,13 +2,16 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
-        const animatedMarker = document.querySelector("#Ja-marker");
+        const animatedMarker = document.querySelector("#ja-marker");
         const aEntity = document.querySelector("#test");
 
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
+                console.console.log(
+                  "clicked"
+                );
                 const scale = aEntity.getAttribute('scale');
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
                 aEntity.setAttribute('scale', scale);
